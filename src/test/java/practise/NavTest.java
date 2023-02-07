c00731949d03e707110ae365350cd45cf8a12b09
CzerwinskiDawid <czerwinski.dawidd@gmail.com>
2023-02-07 17:27:48 +0100

    próba

---
+++ src/test/java/practise/NavTest.java
@@ -0,0 +1,21 @@
+package practise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://coderslab.pl/pl");
+        Thread.sleep(3000);
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        Thread.sleep(3000);
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
